1cece73a36c278e76542c315334935a038055235
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2020-04-17 20:56:05 +0100

    datatypes.h: initial commit

---
+++ include/datatypes.h
@@ -0,0 +1,13 @@
+#ifndef DATATYPES_H
+#define DATATYPES_H
+
+#include <complex>
+#include <vector>
+
+namespace vampost {
+  typedef unsigned int State;
+  typedef std::complex<double> Cplx;
+  typedef std::complex<double> Complex;
+}
+
+#endif
